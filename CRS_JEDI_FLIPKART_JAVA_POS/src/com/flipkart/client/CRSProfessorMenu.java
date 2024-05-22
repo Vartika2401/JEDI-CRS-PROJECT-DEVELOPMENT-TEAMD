2457b7960ffce020fa0117c05ea2beed81e76eaf
rohan2411mitra <rohanmitra2420@gmail.com>
2024-05-22 17:06:50 +0530

    Client Menus

--- CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/client/CRSProfessorMenu.java
+++ CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/client/CRSProfessorMenu.java
@@ -2,10 +2,62 @@
  * 
  */
 package com.flipkart.client;
+import java.util.Scanner;
 
 /**
  * 
  */
 public class CRSProfessorMenu {
+	
+	    public void ProfessorMenu() {
+	        Scanner scanner = new Scanner(System.in);
+	        int choice;
+	 
+	        do {
+	            System.out.println("\nWelcome Professor!\n");
+	            System.out.println("1. My Profile");
+	            System.out.println("2. Courses");
+	            System.out.println("3. Students");
+	            System.out.println("4. Log Out");
+	            System.out.print("\nEnter your choice: ");
+	 
+	            choice = scanner.nextInt();
+	 
+	            switch (choice) {
+	                case 1:
+	                    viewOrUpdateProfile();
+	                    break;
+	                case 2:
+	                    viewCourseOptions();
+	                    break;
+	                case 3:
+	                    viewStudentOptions();
+	                    break;
+	                case 4:
+	                    System.out.println("Logging Out...");
+	                    break;
+	                default:
+	                    System.out.println("Invalid Choice! Please try again.");
+	            }
+	        } while (choice != 4);
+	 
+	        scanner.close();
+	    }
+	 
+	    // Placeholder methods 
+	    public static void viewOrUpdateProfile() {
+	        System.out.println("View/Update Profile (Not implemented yet)");
+	        
+	    }
+	 
+	    public static void viewCourseOptions() {
+	        System.out.println("View Assigned Courses / Course Details (Not implemented yet)");
+	        
+	    }
+	 
+	    public static void viewStudentOptions() {
+	        System.out.println("View Enrolled Students / Upload Grades (Not implemented yet)");
+	        
+	    }
 
 }
--- CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/client/CRSStudentMenu.java
+++ CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/client/CRSStudentMenu.java
@@ -3,9 +3,51 @@
  */
 package com.flipkart.client;
 
+import java.util.Scanner;
+
+
 /**
  * 
  */
 public class CRSStudentMenu {
+	
+	    public void StudentMenu() {
+	        Scanner scanner = new Scanner(System.in);
+	        int choice;
+	 
+	        do {
+	            System.out.println("\nWelcome Student!\n");
+	            System.out.println("1. My Profile");
+	            System.out.println("2. Courses");
+	            System.out.println("3. Grades");
+	            System.out.println("4. Fees");
+	            System.out.println("5. Log Out");
+	            System.out.print("\nEnter your choice: ");
+	 
+	            choice = scanner.nextInt();
+	 
+	            switch (choice) {
+	                case 1:
+	                    System.out.println("My Profile (Not implemented yet)");
+	                    break;
+	                case 2:
+	                    System.out.println("Courses (Not implemented yet)");
+	                    break;
+	                case 3:
+	                    System.out.println("Grades (Not implemented yet)");
+	                    break;
+	                case 4:
+	                    System.out.println("Fees (Not implemented yet)");
+	                    break;
+	                case 5:
+	                    System.out.println("Logging Out...");
+	                    break;
+	                default:
+	                    System.out.println("Invalid Choice! Please try again.");
+	            }
+	        } while (choice != 5);
+	 
+	        scanner.close();
+	    }
 
 }
