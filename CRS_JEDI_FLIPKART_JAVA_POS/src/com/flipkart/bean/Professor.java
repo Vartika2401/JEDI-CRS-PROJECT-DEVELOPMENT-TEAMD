f6add21e3456c43999367f85e9f48cb5dfc9dfbc
rohan2411mitra <rohanmitra2420@gmail.com>
2024-05-22 22:58:21 +0530

    Beans Fixed

--- CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/bean/Professor.java
+++ CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/bean/Professor.java
@@ -1,5 +1,86 @@
 package com.flipkart.bean;
 
-public class Professor {
+import java.util.*;
 
+public class Professor {
+	private String name;
+    private int id;
+    private String emailId;
+    private String department;
+    private List<Integer> selectedCourses;
+    
+//    Constructor
+    public Professor(String name, int id, String emailId, String department) {
+        this.name = name;
+        this.id = id;
+        this.emailId = emailId;
+        this.department = department;
+        this.selectedCourses = new ArrayList<Integer>();
+    }
+
+//    Getters and setters 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public List<Integer> getSelectedCourses() {
+        return selectedCourses;
+    }
+
+    public void setSelectedCourses(List<Integer> selectedCourses) {
+        this.selectedCourses = selectedCourses;
+    }
+
+//    Methods
+    public void getProf() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Email ID: " + emailId);
+        System.out.println("Department: " + department);
+        System.out.println("Selected Courses: " + selectedCourses);
+    }
+
+    public void uploadGrade(int courseId, int studentId, String grade) {
+        System.out.println("Grade '" + grade + "' uploaded for student " + studentId + " in course " + courseId + ".");
+    }
+
+    public void viewEnrolledStudents(int courseId) {
+        System.out.println("Viewing students enrolled in course " + courseId + ".");
+    }
+
+    public void viewCourses() {
+        System.out.println("Courses assigned: " + selectedCourses);
+    }
+
+    public void profLogin(String emailId, String password) {
+        System.out.println("Professor with email '" + emailId + "' is attempting to log in.");
+    }
 }
--- CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/bean/Student.java
+++ CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/bean/Student.java
@@ -1,5 +1,54 @@
 package com.flipkart.bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
+	private String name;
+    private int studentID;
+    private String emailID;
+    private String department;
+    private List<Integer> enrolledCourses;
+    
+//    Constructor
+    public Student(String name, int id, String emailId, String department) {
+        this.name = name;
+        this.studentID = id;
+        this.emailID = emailId;
+        this.department = department;
+        this.enrolledCourses = new ArrayList<Integer>();
+    }
+    
+//    Getter and Setter
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getStudentID() {
+		return studentID;
+	}
+	public void setStudentID(int studentID) {
+		this.studentID = studentID;
+	}
+	public String getEmailID() {
+		return emailID;
+	}
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public List<Integer> getEnrolledCourses() {
+		return enrolledCourses;
+	}
+	public void setEnrolledCourses(List<Integer> enrolledCourses) {
+		this.enrolledCourses = enrolledCourses;
+	}
 
 }
