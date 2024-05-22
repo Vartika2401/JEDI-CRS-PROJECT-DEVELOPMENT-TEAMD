93cc9020ce34a164af16b67a8859a351a7af733d
rohan2411mitra <rohanmitra2420@gmail.com>
2024-05-22 23:06:28 +0530

    professor bean changed

--- CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/bean/Professor.java
+++ CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/bean/Professor.java
@@ -4,83 +4,83 @@ import java.util.*;
 
 public class Professor {
 	private String name;
-    private int id;
-    private String emailId;
-    private String department;
-    private List<Integer> selectedCourses;
-    
+	private int professorID;
+	private String emailID;
+	private String department;
+	private List<Integer> selectedCourses;
+
 //    Constructor
-    public Professor(String name, int id, String emailId, String department) {
-        this.name = name;
-        this.id = id;
-        this.emailId = emailId;
-        this.department = department;
-        this.selectedCourses = new ArrayList<Integer>();
-    }
+	public Professor(String name, int professorID, String emailID, String department) {
+		this.name = name;
+		this.professorID = professorID;
+		this.emailID = emailID;
+		this.department = department;
+		this.selectedCourses = new ArrayList<Integer>();
+	}
 
 //    Getters and setters 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getProfessorID() {
+		return professorID;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int professorID) {
+		this.professorID = professorID;
+	}
 
-    public String getEmailId() {
-        return emailId;
-    }
+	public String getEmailID() {
+		return emailID;
+	}
 
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
-    }
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
 
-    public String getDepartment() {
-        return department;
-    }
+	public String getDepartment() {
+		return department;
+	}
 
-    public void setDepartment(String department) {
-        this.department = department;
-    }
+	public void setDepartment(String department) {
+		this.department = department;
+	}
 
-    public List<Integer> getSelectedCourses() {
-        return selectedCourses;
-    }
+	public List<Integer> getSelectedCourses() {
+		return selectedCourses;
+	}
 
-    public void setSelectedCourses(List<Integer> selectedCourses) {
-        this.selectedCourses = selectedCourses;
-    }
+	public void setSelectedCourses(List<Integer> selectedCourses) {
+		this.selectedCourses = selectedCourses;
+	}
 
 //    Methods
-    public void getProf() {
-        System.out.println("Name: " + name);
-        System.out.println("ID: " + id);
-        System.out.println("Email ID: " + emailId);
-        System.out.println("Department: " + department);
-        System.out.println("Selected Courses: " + selectedCourses);
-    }
+	public void getProf() {
+		System.out.println("Name: " + name);
+		System.out.println("ID: " + professorID);
+		System.out.println("Email ID: " + emailID);
+		System.out.println("Department: " + department);
+		System.out.println("Selected Courses: " + selectedCourses);
+	}
 
-    public void uploadGrade(int courseId, int studentId, String grade) {
-        System.out.println("Grade '" + grade + "' uploaded for student " + studentId + " in course " + courseId + ".");
-    }
+	public void uploadGrade(int courseId, int studentId, String grade) {
+		System.out.println("Grade '" + grade + "' uploaded for student " + studentId + " in course " + courseId + ".");
+	}
 
-    public void viewEnrolledStudents(int courseId) {
-        System.out.println("Viewing students enrolled in course " + courseId + ".");
-    }
+	public void viewEnrolledStudents(int courseId) {
+		System.out.println("Viewing students enrolled in course " + courseId + ".");
+	}
 
-    public void viewCourses() {
-        System.out.println("Courses assigned: " + selectedCourses);
-    }
+	public void viewCourses() {
+		System.out.println("Courses assigned: " + selectedCourses);
+	}
 
-    public void profLogin(String emailId, String password) {
-        System.out.println("Professor with email '" + emailId + "' is attempting to log in.");
-    }
+	public void profLogin(String emailId, String password) {
+		System.out.println("Professor with email '" + emailId + "' is attempting to log in.");
+	}
 }
