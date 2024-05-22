769af95fdc25b7574344d4c35994d34048512306
Vartika2401 <vartika3004@gmail.com>
2024-05-22 21:47:44 +0530

    application client updated

--- CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/client/CRSApplicationClient.java
+++ CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/client/CRSApplicationClient.java
@@ -1,7 +1,14 @@
 /**
  * 
  */
-package com.flipkart.client;
+package CRS_JEDI_FLIPKART_JAVA_POS.src.com.flipkart.client;
+import CRS_JEDI_FLIPKART_JAVA_POS.src.com.flipkart.client.CRSProfessorMenu;
+//import CRS_JEDI_FLIPKART_JAVA_POS.src.com.flipkart.client.CRSStudentMenu;
+//import CRS_JEDI_FLIPKART_JAVA_POS.src.com.flipkart.client.CRSAdminMenu;
+
+
+
+import java.util.Scanner;
 
 /**
  * 
@@ -12,6 +19,35 @@ public class CRSApplicationClient {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println("Welcome to CRS application client");
+		System.out.println("Please enter your choice: \n 1.Login \n 2.Student Registration \n 3.Change Password \n 4.Exit");
+		System.out.print("\nEnter your choice: ");
+		Scanner scanner = new Scanner(System.in);
+		int choice = scanner.nextInt();
+		switch (choice) {
+			case 1:
+				System.out.println("Please enter your username:");
+				String username = scanner.next();
+				System.out.println("Please enter your password:");
+				String password = scanner.next();
+				System.out.println("Choose your role: \n 1.Admin \n 2.Student \n 3.Professor \n 4.Exit");
+				System.out.print("\nEnter your choice: ");
+				int role = scanner.nextInt();
+				switch (role) {
+					case 3:
+					CRSProfessorMenu professorMenu = new CRSProfessorMenu();
+					professorMenu.ProfessorMenu();
+//					case 2:
+//						CRSStudentMenu studentMenu = new CRSStudentMenu();
+//						studentMenu.StudentMenu();
+//					case 1:
+//						com.flipkart.client.CRSAdminMenu adminMenu = new CRSAdminMenu();
+//						adminMenu.adminMenu();
+
+
+				}
+		}
+
 		// TODO Auto-generated method stub
 
 	}
--- CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/client/CRSProfessorMenu.java
+++ CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/client/CRSProfessorMenu.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package com.flipkart.client;
+package CRS_JEDI_FLIPKART_JAVA_POS.src.com.flipkart.client;
 import java.util.Scanner;
 
 /**
