bc639545ef51f089f3bbee23f81ef86fcb5a824d
Vartika2401 <vartika3004@gmail.com>
2024-05-23 09:53:36 +0530

    payment and grade

---
+++ CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/business/gradeMethods.java
@@ -0,0 +1,32 @@
+//package CRS_JEDI_FLIPKART_JAVA_POS.src.com.flipkart.business;
+package com.flipkart.business;
+//
+public class gradeMethods {
+    private String courseID;
+    private String courseName;
+    private String grade;
+
+    public String getCourseCode() {
+        return courseID;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseID = courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
