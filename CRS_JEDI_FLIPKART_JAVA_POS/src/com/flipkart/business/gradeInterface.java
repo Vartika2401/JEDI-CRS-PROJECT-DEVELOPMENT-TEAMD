baeadff114e02438f3fc7ad1d17463f5397a192d
Vartika2401 <vartika3004@gmail.com>
2024-05-23 10:04:55 +0530

    grade and payment interface

---
+++ CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/business/gradeInterface.java
@@ -0,0 +1,17 @@
+//package CRS_JEDI_FLIPKART_JAVA_POS.src.com.flipkart.business;
+package com.flipkart.business;
+
+public interface gradeInterface {
+    public String getCourseCod();
+
+    public void setCourseCode(String courseCode);
+
+    public String getCourseName();
+
+    public void setCourseName(String courseName);
+
+    public String getGrade();
+
+    public void setGrade(String grade);
+
+}
---
+++ CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/business/paymentInterface.java
@@ -0,0 +1,19 @@
+//package CRS_JEDI_FLIPKART_JAVA_POS.src.com.flipkart.business;
+package com.flipkart.business;
+
+
+public interface paymentInterface {
+    public int getStudentID();
+
+    public void setStudentID(int studentID);
+
+    public int getAmount();
+
+    public void setAmount(int amount);
+
+    public String getStatus();
+
+    public void setStatus(String status);
+
+    public void generateConfirmationStatus();
+}
