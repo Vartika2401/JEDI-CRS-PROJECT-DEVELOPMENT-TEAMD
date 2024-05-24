7f82738161acf9d850b5794ef69168dc99d232ad
Rishabh Verma <rayalprajwal82991@gmail.com>
2024-05-25 04:46:30 +0530

    adding admin bean comment

--- CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/bean/Admin.java
+++ CRS_JEDI_FLIPKART_JAVA_POS/src/com/flipkart/bean/Admin.java
@@ -12,12 +12,20 @@ package CRS_JEDI_FLIPKART_JAVA_POS.src.com.flipkart.bean;
  * Prajwal Rayal
  **/
 
-
 public class Admin extends User {
-    // Constructor
-	   public Admin(int id, String name, String phone, String email, String password, String role, String department) {
-	        super(id, name, phone, email, password, role);
-	   }
-    // Additional Admin-specific methods can be added here if needed
+	/**
+	 * Constructor
+	 * @param id The unique identifier for the admin
+	 * @param name The name of the admin
+	 * @param phone The phone number of the admin
+	 * @param email The email address of the admin
+	 * @param password The password for the admin's account
+	 * @param role The role of the admin
+	 * @param department The department the admin belongs to
+	 */
+	public Admin(int id, String name, String phone, String email, String password, String role, String department) {
+		super(id, name, phone, email, password, role);
+	}
 
+	// Additional Admin-specific methods can be added here if needed
 }
